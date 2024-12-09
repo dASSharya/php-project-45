6aad9ed24dd87cca4d2d524ec3d3da5080b8712b
dASSharya <dasharya299@me.com>
2024-12-09 15:06:56 +0300

    q

--- src/cli.php
+++ src/cli.php
@@ -1,20 +1,19 @@
 <?php
+
 namespace src\cli;
 
 use function cli\prompt;
 use function cli\line;
 
 if (!function_exists(__NAMESPACE__ . '\start')) {
-   function start()
-  {
-    line("Welcome to the Brain Games!"); 
+    function start()
+    {
+        line("Welcome to the Brain Games!");
 
-    $name = prompt("May I have your name?");
+        $name = prompt("May I have your name?");
 
-    line("Hello, $name!");
-    
-    return $name;
-  }
+        line("Hello, $name!");
+
+        return $name;
+    }
 }
-
-?>

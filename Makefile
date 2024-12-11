da6ecc824ad768d1f1103e3e7de0ace3955dc448
dASSharya <dasharya299@me.com>
2024-12-11 14:57:24 +0300

    game-even done

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 install:
 	composer install
-brain-games:
+br-games:
 	./bin/brain-games
 validate:
 	composer validate
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+br-even:
+	./bin/brain-even
+
 
---
+++ games/even.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace games\even;
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Подключаем автозагрузчик Composer
+
+
+use function cli\line; // Импортируем функцию line
+use function cli\prompt;
+
+function isItParity($number)
+{
+      return $number % 2 === 0 ? 'yes' : 'no';
+}
+
+if (!function_exists(__NAMESPACE__ . '\start')) {
+    function start()
+    {
+        line("Welcome to the Brain Games!");
+
+        $name = prompt("May I have your name?");
+
+        line("Hello, $name!");
+        return $name;
+    }
+}
+
+function evenGame()
+{
+    $name = start();
+    function description()
+    {
+        line("Answer 'yes' if the number is even, otherwise answer 'no'.");
+    }
+    function randomNumb()
+    {
+        $randomN = rand(1, 100);
+        line("Question: {$randomN}");
+        $answer = prompt("Your answer");
+
+        $correctAnswer = isItParity($randomN);
+
+        if ($answer === $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}. Let's try again, {$name}");
+            exit();
+        }
+    }
+    description();
+    for ($i = 0; $i < 3; $i++) {
+        randomNumb();
+    }
+    line("Congratulations, {$name}!");
+}

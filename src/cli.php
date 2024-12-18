0cacf36a784ef985aea13bf8618c9557f8739030
dASSharya <dasharya299@me.com>
2024-12-18 18:31:46 +0300

    cli changed

--- src/cli.php
+++ src/cli.php
@@ -6,7 +6,7 @@ use function cli\prompt;
 use function cli\line;
 
 function start()
-  {
+{
         line("Welcome to the Brain Games!");
 
         $name = prompt("May I have your name?");
@@ -14,4 +14,4 @@ function start()
         line("Hello, $name!");
 
         return $name;
-  }
+}

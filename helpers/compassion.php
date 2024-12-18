f3ea6e34a76b7c1e4742661f4b7a2f55bbb00f31
dASSharya <dasharya299@me.com>
2024-12-18 19:21:28 +0300

    compassion.php has beeen changed with !

--- helpers/compassion.php
+++ helpers/compassion.php
@@ -14,5 +14,5 @@ use function cli\prompt;
 function wrongAnswer($answer, $correctAnswer, $name) 
 {
   line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}.");
-  line("Let's try again, {$name}");
+  line("Let's try again, {$name}!");
 }
